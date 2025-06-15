7b7ae866c29337d7105efd3671b674b7781e62a8
sagorsenanupam <senanupam10@gmail.com>
2025-06-15 21:09:38 +0600

    edited

---
+++ Payo-mfs/script/add-money.js
@@ -0,0 +1,21 @@
+document
+  .getElementById("add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const amount = parseInt(document.getElementById("amount").value);
+
+    const pinNumber = parseInt(document.getElementById("pin-number").value);
+
+    const mainBalance = parseFloat(
+      document.getElementById("main-balance").innerText
+    );
+
+    if (pinNumber === 1234) {
+      const sum = mainBalance + amount;
+      //   console.log(sum);
+      document.getElementById("main-balance").innerText = sum;
+    } else {
+      alert("Pin Number is Invalid");
+    }
+  });
--- Payo-mfs/script/login.js
+++ Payo-mfs/script/login.js
@@ -1,5 +1,19 @@
 // console.log('Hero aLAM')
 
-document.getElementById('login-btn').addEventListener('click', function(){
-    console.log('Login button clicked');
-})
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const accountNumber = document.getElementById("account-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+
+    if (accountNumber.length === 11) {
+      if (parseInt(pinNumber) === 1234) {
+        window.location.href = "./main.html";
+      } else {
+        alert("you inserted false pin");
+      }
+    } else {
+      alert("Kindly fill up with 11 digit account number");
+    }
+  });
