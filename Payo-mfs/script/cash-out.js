42e95822067f4462868195f7bacece177c61d679
sagorsenanupam <senanupam10@gmail.com>
2025-06-18 22:59:29 +0600

    edited

--- Payo-mfs/script/cash-out.js
+++ Payo-mfs/script/cash-out.js
@@ -1,27 +1,45 @@
+// document
+//   .getElementById("cash-out-btn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     const amount = parseInt(document.getElementById("cash-out-amount").value);
+
+//     const pinNumber = parseInt(
+//       document.getElementById("cash-out-pin-number").value
+//     );
+
+//     const mainBalance = parseFloat(
+//       document.getElementById("main-balance").innerText
+//     );
+
+//     if (pinNumber === 1234) {
+//       if (mainBalance === 0) {
+//         alert("Insufficient Balance");
+//       } else {
+//         const sum = mainBalance - amount;
+//         //   console.log(sum);
+//         document.getElementById("main-balance").innerText = sum;
+//       }
+//     } else {
+//       alert("Pin Number is Invalid");
+//     }
+//   });
+
 document
   .getElementById("cash-out-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
 
-    const amount = parseInt(document.getElementById("cash-out-amount").value);
+    const accountNumber = document.getElementById("account").value;
 
-    const pinNumber = parseInt(
-      document.getElementById("cash-out-pin-number").value
-    );
+    const amount = document.getElementById("cash-out-amount");
+    console.log(amount);
 
-    const mainBalance = parseFloat(
-      document.getElementById("main-balance").innerText
-    );
-
-    if (pinNumber === 1234) {
-      if (mainBalance === 0) {
-        alert("Insufficient Balance");
-      } else {
-        const sum = mainBalance - amount;
-        //   console.log(sum);
-        document.getElementById("main-balance").innerText = sum;
-      }
+    if (accountNumber.length === 11) {
+      // if(){
+      // }
     } else {
-      alert("Pin Number is Invalid");
+      alert("Account Number Invalid");
     }
   });
